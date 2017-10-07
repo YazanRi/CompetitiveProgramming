3f0b079eb9ca53f14c696940c02fc13ae2890563
Yazan Rihani <barca19041@hotmail.com>
2017-10-07 14:06:42 +0200

    edited DICT

--- SPOJ/DICT.cpp
+++ SPOJ/DICT.cpp
@@ -5,36 +5,71 @@
 #define ld long double
 #define IO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 using namespace std;
+string prfx;
+bool f,ff;
+struct trie
+{
+	trie* child[26];
+	bool isleaf;
+	trie()
+	{
+	    isleaf=0;
+		for(int i=0;i<26;i++)
+            child[i]=NULL;
+	}
+	void insert(string &s,int i)
+	{
+		if(i==s.size())
+        {
+            isleaf=1;
+            return;
+        }
+        int cur=s[i]-'a';
+        if(child[cur]==NULL)
+            child[cur]=new trie();
+        child[cur]->insert(s,i+1);
+	}
+	void find(string &s,int i)
+	{
+	    if(i==s.size())
+        {
+            ff=0;
+            travel("");
+            return;
+        }
+	    int cur=s[i]-'a';
+	    if(child[cur]==NULL)return;
+	    child[cur]->find(s,i+1);
+	}
+	void travel(string s)
+	{
+	    if(isleaf&&ff)
+            cout<<prfx<<s<<'\n',f=1;
+        ff=1;
+        for(int i=0;i<26;i++)
+            if(child[i]!=NULL)
+                child[i]->travel(s+char(i+'a'));
+	}
+
+}t;
 int n,k;
-string s,a[25050];
-map<string,bool>vis;
-map<string,vector<int> >mp;
+string s;
 int main()
 {
     IO;
     cin>>n;
     for(int i=0;i<n;i++)
-        cin>>a[i];
-    sort(a,a+n);
-    for(int i=0;i<n;i++)
     {
-        if(vis.find(a[i])!=vis.end())continue;
-        vis[a[i]]=1;
-        string x="";
-        for(int j=0;j<a[i].size()-1;j++)
-        {
-            x+=a[i][j];
-            mp[x].push_back(i);
-        }
+        cin>>s;
+        t.insert(s,0);
     }
     cin>>k;
     for(int tc=1;tc<=k;tc++)
     {
-        cin>>s;
+        cin>>prfx;
+        f=0;
         cout<<"Case #"<<tc<<":\n";
-        auto it=mp.find(s);
-        if(it==mp.end())cout<<"No match.\n";
-        else for(int i=0;i<(it->s.size());i++)
-                cout<<a[(it->s)[i]]<<'\n';
+        t.find(prfx,0);
+        if(!f)cout<<"No match.\n";
     }
 }
